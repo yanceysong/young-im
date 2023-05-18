a73f53acfbf44c871af64c594cb84cff463519da
songyang <1437370865@qq.com>
2023-05-18 13:32:58 +0800

    修正mapper扫描路径错误

--- im-message-store/src/main/java/com/yanceysong/im/message/Application.java
+++ im-message-store/src/main/java/com/yanceysong/im/message/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.yancyesong.im.message.dao.mapper")
+@MapperScan("com.yanceysong.im.message.dao.mapper")
 public class Application {
 
     public static void main(String[] args) {
